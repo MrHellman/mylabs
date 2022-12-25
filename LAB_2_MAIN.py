c42b6bf7de8a1c18c87ef5d3ab864bb2c6eec294
MrHellman <117468213+MrHellman@users.noreply.github.com>
2022-12-26 01:43:47 +0300

    V2

--- LAB_2_MAIN.py
+++ LAB_2_MAIN.py
@@ -1,10 +1,11 @@
 import requests
-s_city ="Moscow,RU"
-appid ="c0dfa5e8162869bf658580cda4daa1f8"
-res = requests.get("http://api.openweathermap.org/data/2.5/weather",params={'q': s_city,'units':'metric','lang':'ru','APPID': appid})
+city = "Moscow,RU"
+appid = "c0dfa5e8162869bf658580cda4daa1f8"
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+    params={"q": city, "units": "metric", "lang": "ru", "APPID": appid})
 data = res.json()
-print(data['main'])
-print("Город:", s_city)
+print(data)
+print("Город:", city)
 print("Погодные условия:", data['weather'][0]['description'])
 print("Температура:", data['main']['temp'])
 print("Ощущается как:", data['main']['feels_like'])
@@ -13,10 +14,21 @@ print("Максимальная температура", data['main']['temp_max'
 print("Давление:", data['main']['pressure'])
 print('Слила ветра сегодня:', )
 print("Прогноз погоды на неделю:")
-for i in data['list']:
-    print("Дата <", i['dt_txt'],"> \r\nТемпература<",
-        '{0:+3.0f}'.format(i['main']['temp']),"> \r\nПогодныеусловия <",
-        i['weather'][0]['description'], "> \r\nСила ветра на сегодня <",
-        i['wind']['speed'], 'м/с', '> \r\nВидимость на сегодня <',
-        i['visibility'] / 10000 * 100, '%', ">")
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+    params={"q": city, "units": "metric", "lang": "ru", "APPID": appid})
+data = res.json()
+print(f"\nПрогноз погоды на неделю:")
+for i in data["list"]:
+    print(f"={i ['dt_txt']}")
+    print(f"Температура              {i['main']['temp']:+.0f}")
+    print(f"Минимальная температура  {i['main']['temp_min']}")
+    print(f"Максимальная температура {i['main']['temp_max']}")
+    print(f"Ощущается как            {i['main']['feels_like']}")
+    print(f"Погодные условия         {i['weather'][0]['description']}")
+    print("____________________________")
+print("Прогноз ветра и видимости на неделю:")
+for i in data["list"]:
+    print("Дата: ", i['dt_txt'])
+    print("Скорость ветра: ", i['wind']['speed'], ' м/с')
+    print("Видимость:      ", round((i['visibility'] / 10000) * 100), '%')
     print("____________________________")
